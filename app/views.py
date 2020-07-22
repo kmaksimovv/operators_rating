b8f577d7189ca32aa6d4003c5afacd3557268990
Максимов Константин <kmaksimovv@gmail.com>
2020-07-22 12:51:58 +0400

    fixes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
+import os
 from app import app
 from app import db
-from app import excel
 from flask import render_template, url_for
 from .models import Rating
 from .forms import *
@@ -8,50 +8,42 @@ from sqlalchemy import func
 from datetime import date, datetime
 from flask import request, redirect
 from flask import send_file
+from xlsxwriter.workbook import Workbook
+from flask import after_this_request
 
 @app.route('/results')
 def search_results(search):
     ratings = []
     form = SearchForm(request.form)
+    operator = request.form.get('operator').strip()
+    callerid = request.form.get('callerid').strip()
+    start_date = request.form.get('start_date').strip()
+    end_date = request.form.get('end_date').strip()
     
-    if search.data['operator'] and search.data['callerid'] and search.data['start_date'] and search.data['end_date']:
-        search_string_operator = search.data['operator'].strip()
-        search_string_callerid = search.data['callerid'].strip()
-        search_start_date = search.data['start_date']
-        search_end_date = search.data['end_date']
-        qry = db.session.query(Rating).filter(Rating.operator == search_string_operator, Rating.callerid == search_string_callerid).filter(Rating.created_at.between(search_start_date, search_end_date))
-        ratings = qry.all()
-        return render_template('index.html', ratings=ratings, form=form)
-    elif search.data['operator'] and search.data['start_date'] and search.data['end_date']:
-        search_string_operator = search.data['operator'].strip()
-        search_start_date = search.data['start_date']
-        search_end_date = search.data['end_date']
-        qry = db.session.query(Rating).filter(Rating.operator == search_string_operator, Rating.created_at.between(search_start_date, search_end_date))
-        ratings = qry.all()
-        return render_template('index.html', ratings=ratings, form=form)
-    elif search.data['callerid'] and search.data['start_date'] and search.data['end_date']:
-        search_string = search.data['callerid'].strip()
-        search_start_date = search.data['start_date']
-        search_end_date = search.data['end_date']
-        qry = db.session.query(Rating).filter(Rating.callerid == search_string_callerid, Rating.created_at.between(search_start_date, search_end_date))
-        ratings = qry.all()
-        return render_template('index.html', ratings=ratings, form=form)        
-    elif search.data['operator'] and search.data['callerid']:
-        search_string_operator = search.data['operator'].strip()
-        search_string_callerid = search.data['callerid'].strip()
-        qry = db.session.query(Rating).filter(Rating.operator == search_string_operator, Rating.callerid == search_string_callerid)
-        ratings = qry.all()
-        return render_template('index.html', ratings=ratings, form=form)    
-    elif search.data['operator']:
-        search_string = search.data['operator'].strip()
-        qry = db.session.query(Rating).filter(Rating.operator == search_string)
-        ratings = qry.all()
-        return render_template('index.html', ratings=ratings, form=form)
-    elif search.data['callerid']:
-        search_string = search.data['callerid'].strip()
-        qry = db.session.query(Rating).filter(Rating.callerid == search_string)
-        ratings = qry.all()
-        return render_template('index.html', ratings=ratings, form=form)
+    if operator and callerid and start_date and end_date:
+        query_sets = db.session.query(Rating).filter(Rating.operator == operator, Rating.callerid == callerid).filter(Rating.created_at.between(start_date, end_date)).all()
+
+        return render_template('index.html', ratings=query_sets, form=form)
+    elif operator and callerid:
+        query_sets = db.session.query(Rating).filter_by(operator=operator, callerid=callerid).all()
+
+        return render_template('index.html', ratings=query_sets, form=form)
+    elif operator and start_date and end_date:
+        query_sets = db.session.query(Rating).filter_by(operator=operator).filter(Rating.created_at.between(start_date, end_date)).all()
+
+        return render_template('index.html', ratings=query_sets, form=form)
+    elif callerid and start_date and end_date:
+        query_sets = db.session.query(Rating).filter_by(callerid=callerid).filter(Rating.created_at.between(start_date, end_date)).all()
+        
+        return render_template('index.html', ratings=query_sets, form=form)
+    elif operator:
+        query_sets = db.session.query(Rating).filter_by(operator=operator).all()
+        
+        return render_template('index.html', ratings=query_sets, form=form)
+    elif callerid:
+        query_sets = db.session.query(Rating).filter_by(callerid=callerid).all()
+        
+        return render_template('index.html', ratings=query_sets, form=form)
     else:
         return redirect(url_for('index'))
         
@@ -68,7 +60,8 @@ def index():
 
 @app.route('/graph')
 def graph_all():
-    ratings = db.session.query(Rating).order_by(db.asc(Rating.created_at)).limit(15).all()
+    ratings = db.session.query(Rating).order_by(db.desc(Rating.id)).limit(15).all()
+    ratings.reverse()
     labels = []
     values = []
     
@@ -79,7 +72,8 @@ def graph_all():
 
 @app.route('/graph-today')
 def graph_today():
-    ratings = db.session.query(Rating).order_by(db.asc(Rating.created_at)).limit(15).all()
+    ratings = db.session.query(Rating).order_by(db.desc(Rating.created_at)).limit(15).all()
+    ratings.reverse()
     labels = []
     values = []
     
@@ -90,7 +84,8 @@ def graph_today():
 
 @app.route('/graph-yesterday')
 def graph_yesterday():
-    ratings = db.session.query(Rating).order_by(db.asc(Rating.created_at)).limit(15).all()
+    ratings = db.session.query(Rating).order_by(db.desc(Rating.created_at)).limit(15).all()
+    ratings.reverse()
     labels = []
     values = []
     
@@ -99,44 +94,7 @@ def graph_yesterday():
         values.append(r.value)
     return render_template('graph.html', labels=labels, values=values)
 
-@app.route("/export", methods=['GET'])
-def doexport():
-    return excel.make_response_from_a_table(db.session, Rating, "xlsx", file_name="sheet")
 
-@app.route('/custom_export_report')
-def custom_export_report(form):
-    operator = request.form.get('operator').strip()
-    callerid = request.form.get('callerid').strip()
-    start_date = request.form.get('start_date').strip()
-    end_date = request.form.get('end_date').strip()
-    column_names = ['id', 'operator', 'queue', 'callerid', 'value', 'created_at']
-
-    if operator and callerid and start_date and end_date:
-        query_sets = db.session.query(Rating).filter(Rating.operator == operator, Rating.callerid == callerid).filter(Rating.created_at.between(start_date, end_date)).all()
-        return excel.make_response_from_query_sets(query_sets, column_names,"xlsx", file_name=f"report_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}")
-    elif operator and callerid:
-        query_sets = db.session.query(Rating).filter_by(operator=operator, callerid=callerid).all()
-        return excel.make_response_from_query_sets(query_sets, column_names,"xlsx", file_name=f"report_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}")
-    elif start_date and end_date:
-        query_sets = db.session.query(Rating).filter(Rating.created_at.between(start_date, end_date)).all()
-        return excel.make_response_from_query_sets(query_sets, column_names,"xlsx", file_name=f"report_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}")
-    elif operator:
-        query_sets = db.session.query(Rating).filter_by(operator=operator).all()
-        return excel.make_response_from_query_sets(query_sets, column_names,"xlsx", file_name=f"report_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}")
-    elif callerid:
-        query_sets = db.session.query(Rating).filter_by(callerid=callerid).all()
-        return excel.make_response_from_query_sets(query_sets, column_names,"xlsx", file_name=f"report_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}")
-    else:
-        return render_template("report.html", form=form)
-
-@app.route("/custom_export", methods=['GET', 'POST'])
-def custom_export():
-    form = SearchForm()
-    
-    if request.method == 'POST':
-        return custom_export_report(form)
-    
-    return render_template("report.html", form=form)
 
 @app.route("/rating-by-operator", methods=['GET'])
 def rating_by_operator():
@@ -147,21 +105,23 @@ def rating_by_operator():
         rat = Rating()
         rat.id = index
         rat.operator = operator 
-        rat.labels = [r[0].strftime("%m-%d %H:%M") for r in db.session.query(Rating).order_by(db.asc(Rating.created_at)).filter_by(operator=operator).limit(10).values('created_at')]
-        rat.values = [r[0] for r in db.session.query(Rating).order_by(db.asc(Rating.created_at)).filter_by(operator=operator).limit(10).values('value')]
+        rat.labels = [r[0].strftime("%m-%d %H:%M:%S") for r in db.session.query(Rating).order_by(db.desc(Rating.id)).filter_by(operator=operator).limit(10).values('created_at')]
+        rat.values = [r[0] for r in db.session.query(Rating).order_by(db.desc(Rating.id)).filter_by(operator=operator).limit(10).values('value')]
         rating_by_operators.append(rat)
     
+    for r in rating_by_operators:
+        r.labels.reverse()
+        r.values.reverse()
+      
     return render_template('rating_by_operator.html', ratings=rating_by_operators)
 
 @app.route('/get_xslx_for_data')
-def get_xslx_for_data():
-    import xlsxwriter
-    from xlsxwriter.workbook import Workbook
-    from io import BytesIO
+def get_xslx_for_data(ratings):
+
+    
     name_file = f"report_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.xlsx"
-    workbook = xlsxwriter.Workbook(name_file)
-    worksheet = workbook.add_worksheet()
-    ratings = db.session.query(Rating).all()
+    workbook = Workbook(name_file)
+    worksheet = workbook.add_worksheet("Оценка")
     worksheet.write(0,0, "ид")
     worksheet.write(0,1, "оператор")
     worksheet.write(0,2, "очередь")
@@ -181,8 +141,58 @@ def get_xslx_for_data():
         row += 1
         
     
-    workbook.close()   
+    workbook.close()
+
+    @after_this_request
+    def remove_file(response):
+        try:
+            os.remove(name_file)
+        except Exception as error:
+            app.logger.error("Error removing or closing downloaded file handle", error)
+        return response
     
     return send_file('../' + name_file, as_attachment=True, cache_timeout=0)
 
 
+@app.route('/custom_export_report')
+def custom_export_report(form):
+    operator = request.form.get('operator').strip()
+    callerid = request.form.get('callerid').strip()
+    start_date = request.form.get('start_date').strip()
+    end_date = request.form.get('end_date').strip()
+    
+    if operator and callerid and start_date and end_date:
+        query_sets = db.session.query(Rating).filter(Rating.operator == operator, Rating.callerid == callerid).filter(Rating.created_at.between(start_date, end_date)).all()
+
+        return get_xslx_for_data(query_sets)
+    elif operator and callerid:
+        query_sets = db.session.query(Rating).filter_by(operator=operator, callerid=callerid).all()
+
+        return get_xslx_for_data(query_sets)
+    elif operator and start_date and end_date:
+        query_sets = db.session.query(Rating).filter_by(operator=operator).filter(Rating.created_at.between(start_date, end_date)).all()
+
+        return get_xslx_for_data(query_sets)
+    elif callerid and start_date and end_date:
+        query_sets = db.session.query(Rating).filter_by(callerid=callerid).filter(Rating.created_at.between(start_date, end_date)).all()
+
+        return get_xslx_for_data(query_sets)
+    elif operator:
+        query_sets = db.session.query(Rating).filter_by(operator=operator).all()
+
+        return get_xslx_for_data(query_sets)
+    elif callerid:
+        query_sets = db.session.query(Rating).filter_by(callerid=callerid).all()
+
+        return get_xslx_for_data(query_sets)
+    else:
+        return render_template("report.html", form=form)
+    
+@app.route("/custom_export", methods=['GET', 'POST'])
+def custom_export():
+    form = SearchForm()
+    
+    if request.method == 'POST':
+        return custom_export_report(form)
+    
+    return render_template("report.html", form=form)
