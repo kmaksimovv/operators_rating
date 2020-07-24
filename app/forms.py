a180cb28ccf46ec85b14727e47ceaaa0c617b88a
Максимов Константин <kmaksimovv@gmail.com>
2020-07-24 17:16:20 +0400

    test datetimepicker

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.fields.html5 import DateField
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField,  DateTimeField
+from wtforms.fields.html5 import DateTimeLocalField
 
 class SearchForm(FlaskForm):
     operator = StringField("Оператор:")
     callerid = StringField("По номеру:")
     start_date = DateField('Начало Даты')
     end_date = DateField('Конец Даты')
-
